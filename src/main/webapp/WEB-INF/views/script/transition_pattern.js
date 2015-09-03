3b734d0d29180f3ae8212e95d41e799a4c55d734
h.satomi <h.satomi@souya.biz>
2015-09-03 19:13:50 +0900

    プッシュ漏れ

---
+++ src/main/webapp/WEB-INF/views/script/transition_pattern.js
@@ -0,0 +1,15 @@
+﻿window.onload = function () {
+    // ページ読み込み時に実行したい処理
+    document.getElementById("statistics_information").style.width = window.parent.screen.width * 0.4 + "px";
+    document.getElementById("statistics_information").style.height = window.parent.screen.height * 0.8 + "px";
+
+    document.getElementById("parameter_pattern").style.width = window.parent.screen.width * 0.5 + "px";
+    document.getElementById("parameter_pattern").style.height = window.parent.screen.height * 0.8 + "px";
+}
+
+function move(mode) {
+    switch (mode) {
+        case "parameter_pattern": location.href = "parameter_pattern.jsp"; break;
+        default: break;
+    }
+}
