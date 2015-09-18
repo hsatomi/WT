864d458571e324370fe34ab6a3399394ec4b27d6
h.satomi <h.satomi@souya.biz>
2015-09-18 13:06:23 +0900

    いったんコメントアウト gitはりようしない

--- auto_git_for_wt.sh
+++ auto_git_for_wt.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #内容を自動でローカルリポジトリにPUSHするスクリプト
 
-cd /usr/local/src/wt/
-git add .
-git commit -m "auto by test tool"
-sudo git push origin2 master
+#動かないのでコメントアウト
+#cd /usr/local/src/wt/
+#git add .
+#git commit -m "auto by test tool"
+#sudo git push origin2 master
